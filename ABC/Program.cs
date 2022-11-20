c0653bb13c6a01918bf356cca5f37c2705b4a370
RAFAEL AUGUSTO TORRES PAULINO <rtorrespaulino@outlook.com>
2022-11-20 10:24:56 -0400

    Imprime el Abecedario de A-Z

---
+++ ABC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int abc = 'a'; abc <= 'z'; abc++)
+            {
+                Console.WriteLine((char)abc+"*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
